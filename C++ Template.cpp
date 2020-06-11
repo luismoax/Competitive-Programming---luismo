84049c5d4bd939b794e79b9468955f269ffd6312
Luis Manuel <luismoax@gmail.com>
2020-06-11 00:31:39 -0400

    update C++ Template

--- C++ Template.cpp
+++ C++ Template.cpp
@@ -1,9 +1,9 @@
 /*
 	Author: Luis Manuel Díaz Barón (LUISMO)
-	Problem:
-	Link:
+	Problem: 
+	Link: 
 	Online Judge: 
-	Idea:
+	Idea: 
 */
 #include<bits/stdc++.h>
 // Types
@@ -14,30 +14,29 @@
 #define sf scanf
 #define pf printf
 #define mkp make_pair
-#define fi first
-#define se second
+#define x first
+#define y second
 #define endl "\n"
-
+ 
 using namespace std;
-
+ 
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-const ll inf = 1e16 + 3;
+const ll inf = 1e18;
 const int mod = 1e9 + 7;
-const int lim = 2e5 + 2;
-
+const int lim = 2e5 + 3;
 
 void solve()
 {
-
+	
 }
-
+ 
 void fastIO()
 {
 	cin.sync_with_stdio(false);
 	cin.tie(0);
 }
-
+ 
 void IO()
 {
 	if(fopen("c:\\Users\\LUISMO\\lmo.in","r") != NULL)
@@ -53,12 +52,12 @@ void IO()
 		freopen("/media/Beijing/lmo.in", "r", stdin);
 	}
 }
-
+ 
 int main()
 {
 	IO();
-
+ 
 	fastIO();
-
+ 
 	solve();
 }
