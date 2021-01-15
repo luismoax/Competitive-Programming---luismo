c700c3713e8dc5bc8d926b070dba0e7e5c61bc03
Luis Manuel <luismoax@gmail.com>
2021-01-14 22:19:37 -0500

    refactor Segment Tree on Subarray Sum Queries

--- CSES/Range Queries/Subarray Sum Queries.cpp
+++ CSES/Range Queries/Subarray Sum Queries.cpp
@@ -37,33 +37,36 @@ const ll lim = 2e5 + 3;
 
 int n, m, k, x;
 ll arr[lim];
+
+// >> SEGMENT TREE
 struct node
 {
 	ll sum; // sum of all values in the interval
 	ll bestSum; // best sum of the interval
 	ll bestLeft; // best sum of a prefix in the interval
 	ll bestRight; // best sum of a sufix in the interval
-
+ 
 	node(){sum = bestLeft = bestRight = bestSum = 0;}
 	node(ll sm, ll bs, ll bl, ll br)
 	{ sum = sm; bestSum = bs; bestLeft = bl; bestRight = br; }
 };
 node t[lim << 2];
-
-void merge(int idx, int L, int R)
+ 
+node merge(node leNode, node riNode)
 {
-	int le = idx * 2 + 1, ri = le + 1;
-	t[idx].sum = t[le].sum + t[ri].sum;
-	t[idx].bestSum = max(t[le].bestSum, t[ri].bestSum);
+    node ret;
+	ret.sum = leNode.sum + riNode.sum;
+	ret.bestSum = max(leNode.bestSum, riNode.bestSum);
+ 
+	if(leNode.bestRight + riNode.bestLeft > ret.bestSum)
+		ret.bestSum = leNode.bestRight + riNode.bestLeft;
+ 
+	ret.bestLeft = max(leNode.bestLeft, leNode.sum + riNode.bestLeft);
+	ret.bestRight = max(riNode.bestRight, riNode.sum + leNode.bestRight);
 
-	if(t[le].bestRight + t[ri].bestLeft > t[idx].bestSum)
-		t[idx].bestSum = t[le].bestRight + t[ri].bestLeft;
-
-	t[idx].bestLeft = max(t[le].bestLeft, t[le].sum + t[ri].bestLeft);
-	t[idx].bestRight = max(t[ri].bestRight, t[ri].sum + t[le].bestRight);
+	return ret;
 }
 
-// >> SEGMENT TREE
 void buildTree(int idx, int L, int R)
 {
 	int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
@@ -75,9 +78,9 @@ void buildTree(int idx, int L, int R)
 	buildTree(le, L, mid);
 	buildTree(ri, mid + 1, R);
 	// merge
-	merge(idx, L, R);
+	t[idx] = merge(t[le], t[ri]);
 }
-
+ 
 void update(int idx, int L, int R, int ii, int upd)
 {
 	int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
@@ -92,13 +95,35 @@ void update(int idx, int L, int R, int ii, int upd)
 	else 
 		update(ri, mid + 1, R, ii, upd);
 	// merge
-	merge(idx, L, R);
+	t[idx] = merge(t[le], t[ri]);
+}
+
+node retrieve(int idx, int L, int R, int QL, int QR)
+{
+	int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
+	if(L >= QL && R <= QR)
+		return t[idx];
+	if(L > QR || R < QL)
+		return node(-inf, -inf, -inf, -inf);
+
+	node leNode = retrieve(le, L, mid, QL, QR);
+	node riNode = retrieve(ri, mid + 1, R, QL, QR);
+
+	if(riNode.sum == -inf)
+		return leNode;
+	if(leNode.sum == -inf)
+		return riNode;
+
+	node ret = merge(leNode, riNode);
+
+	return ret;
 }
 // << SEGMENT TREE
 
 void solve()
 {
 	cin >> n >> m;
+
 	for(int i = 0; i < n; i++)
 		cin >> arr[i];
 
@@ -123,9 +148,9 @@ void fastIO()
  
 void IO()
 {
-	if(fopen("c:\\Users\\LUISMO\\lmo.in","r") != NULL)
+	if(fopen("c:\\Competitive Programming\\lmo.in","r") != NULL)
 	{
-		freopen("c:\\Users\\LUISMO\\lmo.in","r",stdin);
+		freopen("c:\\Competitive Programming\\lmo.in","r",stdin);
 	}
 	else if(fopen("d:\\lmo.in","r") != NULL)
 	{
