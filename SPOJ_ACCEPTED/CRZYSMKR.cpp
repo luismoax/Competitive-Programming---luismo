3333442236ab923b9a9f479ac74ecdf80e4aae5a
Luis Mannuel <luismoax@gmail.com>
2021-03-25 09:30:49 -0400

    Add CRZYSMKR.cpp to SPOJ folder

---
+++ SPOJ_ACCEPTED/CRZYSMKR.cpp
@@ -0,0 +1,80 @@
+/*
+	Author: Luis Manuel Díaz Barón (LUISMO)
+	Problem: CRZYSMKR
+	Link: https://www.spoj.com/problems/CRZYSMKR/
+	Online Judge: SPOJ
+	Idea: Simple modular math
+*/
+#include<bits/stdc++.h>
+// Types
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+// IO
+#define sf scanf
+#define pf printf
+#define mkp make_pair
+#define fi first
+#define se second
+#define M_PI 2.0 * acos(0)
+#define endl "\n"
+ 
+using namespace std;
+ 
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const ll inf = 1e18;
+const int mod = 1e9 + 7;
+const int lim = 2e5 + 3;
+
+int tc;
+ull N;
+
+void solve()
+{
+	cin >> tc;
+
+	while(tc--> 0)
+	{
+		cin >> N;
+
+		N %=11;
+
+		ull M = 11 - ((8 * N) % 11);
+
+		cout << M % 11 << endl;
+	}	
+}
+ 
+void fastIO()
+{
+	cin.sync_with_stdio(false);
+	cin.tie(0);
+}
+ 
+void IO()
+{
+
+	if(fopen("c:\\Competitive Programming\\lmo.in","r") != NULL)
+	{
+		freopen("c:\\Competitive Programming\\lmo.in","r",stdin);
+	}
+	else if(fopen("d:\\lmo.in","r") != NULL)
+	{
+		freopen("d:\\lmo.in","r",stdin);
+		// freopen("d:\\lmo.out","w",stdout);
+	}
+	else if(fopen("/Wolfburg/lmo.in","r") != NULL)
+	{
+		freopen("/Wolfburg/lmo.in", "r", stdin);
+	}
+}
+ 
+int main()
+{
+	IO();
+ 
+	fastIO();
+ 
+	solve();
+}
