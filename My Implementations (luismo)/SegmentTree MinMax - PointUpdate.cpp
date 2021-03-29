f85923d09261dcac1cfe2c2aae00a1eb3b9228d7
Luis Mannuel <luismoax@gmail.com>
2021-03-28 22:53:17 -0400

    Enhance implementation, wrapping it on a struct using vectors

--- My Implementations (luismo)/SegmentTree MinMax - PointUpdate.cpp
+++ My Implementations (luismo)/SegmentTree MinMax - PointUpdate.cpp
@@ -1,38 +1,48 @@
 
 // MIN / MAX SegmentTree - Point Update
-
-int mx[lim << 2];
-int mn[lim << 2];
-
-void update(int idx, int L, int R, int ii, int upd)
+struct SegmentTree
 {
-    if(L == R)
-    {
-        mx[idx] = mn[idx] = upd;
-        return;
-    }
-    int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
-    if(ii <= mid)
-        update(le, L, mid, ii, upd);
-    else update(ri, mid + 1, R, ii, upd);
-    // merge
-    mx[idx] = max(mx[le], mx[ri]);
-    mn[idx] = min(mn[le], mn[ri]);
-}
 
-pii retrieve(int idx, int L, int R, int QL, int QR)
-{
-    if(L >= QL && R <= QR)
-        return mkp(mx[idx], mn[idx]);
+	vector<int> mx;
+	vector<int> mn;
 
-    if(L > QR || R < QL)
-        return mkp(-1, mod + 59);
+	SegmentTree(int len)
+	{
+		mx.resize(len << 2 + 3, 0);
+		mn.resize(len << 2 + 3, 0);
+	}
 
-    int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
+	void update(int idx, int L, int R, int ii, int upd)
+	{
+		if(L == R)
+		{
+			mx[idx] = mn[idx] = upd;
+			return;
+		}
+		int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
+		if(ii <= mid)
+			update(le, L, mid, ii, upd);
+		else update(ri, mid + 1, R, ii, upd);
+		// merge
+		mx[idx] = max(mx[le], mx[ri]);
+		mn[idx] = min(mn[le], mn[ri]);
+	}
 
-    pii s1 = retrieve(le, L, mid, QL, QR);
-    pii s2 = retrieve(ri, mid + 1, R, QL, QR);
-    // merge
-    pii ret = mkp(max(s1.first, s2.first), min(s1.second, s2.second));
-    return ret;
-}
+	pii retrieve(int idx, int L, int R, int QL, int QR)
+	{
+		if(L >= QL && R <= QR)
+			return mkp(mx[idx], mn[idx]);
+
+		if(L > QR || R < QL)
+			return mkp(-1, mod + 59);
+
+		int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
+
+		pii s1 = retrieve(le, L, mid, QL, QR);
+		pii s2 = retrieve(ri, mid + 1, R, QL, QR);
+		// merge
+		pii ret = mkp(max(s1.first, s2.first), min(s1.second, s2.second));
+		return ret;
+	}
+
+};
