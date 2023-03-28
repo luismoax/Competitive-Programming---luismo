8dc3e792d4d783170ed8dddc8cf54f0a6ec14f22
Luis Manuel Díaz <luismoax@gmail.com>
2023-03-27 21:47:48 -0400

    Update SegmentTree MinMax - PointUpdate to use Long Long

--- My Implementations (luismo)/SegmentTree MinMax - PointUpdate.cpp
+++ My Implementations (luismo)/SegmentTree MinMax - PointUpdate.cpp
@@ -1,10 +1,9 @@
-
 // MIN / MAX SegmentTree - Point Update
 struct SegmentTree
 {
 
-	vector<int> mx;
-	vector<int> mn;
+	vector<ll> mx;
+	vector<ll> mn;
 	int SZ;
 
 	SegmentTree(int len)
@@ -14,12 +13,12 @@ struct SegmentTree
 		mn.resize(len << 2 + 3, 0);
 	}
 
-	void update(int ii, int upd)
+	void update(int ii, ll upd)
 	{
 		update(0, 0, SZ - 1, ii, upd);
 	}
 
-	void update(int idx, int L, int R, int ii, int upd)
+	void update(int idx, int L, int R, int ii, ll upd)
 	{
 		if(L == R)
 		{
@@ -35,25 +34,25 @@ struct SegmentTree
 		mn[idx] = min(mn[le], mn[ri]);
 	}
 
-	pii retrieve(int QL, int QR)
+	pll retrieve(int QL, int QR)
 	{
 		return retrieve(0, 0, SZ - 1, QL, QR);
 	}
 
-	pii retrieve(int idx, int L, int R, int QL, int QR)
+	pll retrieve(int idx, int L, int R, int QL, int QR)
 	{
 		if(L >= QL && R <= QR)
 			return mkp(mx[idx], mn[idx]);
 
 		if(L > QR || R < QL)
-			return mkp(-1, mod + 59);
+			return mkp(-inf, inf);
 
 		int le = idx * 2 + 1, ri = le + 1, mid = (L + R) / 2;
 
-		pii s1 = retrieve(le, L, mid, QL, QR);
-		pii s2 = retrieve(ri, mid + 1, R, QL, QR);
+		pll s1 = retrieve(le, L, mid, QL, QR);
+		pll s2 = retrieve(ri, mid + 1, R, QL, QR);
 		// merge
-		pii ret = mkp(max(s1.first, s2.first), min(s1.second, s2.second));
+		pll ret = mkp(max(s1.first, s2.first), min(s1.second, s2.second));
 		return ret;
 	}
 };
